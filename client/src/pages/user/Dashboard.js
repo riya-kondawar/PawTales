34ab043a5656953b16a30264128c869172f092c3
riiya-kondawar <riya.kondawar@gmail.com>
2024-10-21 09:49:23 +0530

    update R

---
+++ client/src/pages/user/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout.js'
+
+const Dashboard = () => {
+  return (
+    <div title={'Dashboard - PawTales'}>
+      <h1>USER DASHBOARD Page</h1>
+    </div>
+  )
+}
+
+export default Dashboard
