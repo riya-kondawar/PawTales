d86fe83c01d2ab9cf5349f5ce6901f6d555b48e2
riiya-kondawar <riya.kondawar@gmail.com>
2024-10-24 22:49:22 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,26 @@ import Policy from "./pages/Policy.js";
 import PageNotFound from "./pages/PageNotFound.js";
 import Register from "./pages/Auth/Register.js";
 import Login from "./pages/Auth/Login.js";
-import Pets from "./pages/Pets.js";
 import Dashboard from "./pages/user/Dashboard.js";
+import PrivateRoute from "./components/Routes/Private.js";
+import ForgotPassword from "./pages/Auth/ForgotPassword.js";
 import AdminRoute from "./components/Routes/AdminRoutes.js";
 import AdminDashboard from "./pages/Admin/AdminDashboard.js";
-import ForgotPassword from "./pages/Auth/ForgotPassword.js";
-import PrivateRoute from "./components/Routes/Private.js";
 import CreateCategory from "./pages/Admin/CreateCategory.js";
 import CreateProduct from "./pages/Admin/CreateProduct.js";
 import Users from "./pages/Admin/Users.js";
-import Adoption from "./pages/user/Adoption.js";
+import Adoption from "./pages/user/Adoption.js"; // Orders
 import Profile from "./pages/user/Profile.js";
+// import Products from "./pages/Admin/Products.js";
+// update product
+import Search from "antd/es/transfer/search.js";
+// product details
+// Categories
+// Categ Product
+
+import CartPage from "./pages/CartPage";
+// import Pets from "./pages/Pets.js";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-// import Pets from "./pages/user/Pets.js";
 
 function App() {
   return (
@@ -27,6 +33,11 @@ function App() {
       <ToastContainer />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        {/* <Route path="/product/:slug" element={<ProductDetails />} /> */}
+        {/* <Route path="/categories" element={<Categories />} /> */}
+        <Route path="/cart" element={<CartPage />} />
+        {/* <Route path="/category/:slug" element={<CategoryProduct />} /> */}
+        <Route path="/search" element={<Search />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
           <Route path="user/adoption" element={<Adoption />} />
@@ -36,6 +47,7 @@ function App() {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
+          {/* <Route path="admin/products" element={<Products />} /> */}
           <Route path="admin/users" element={<Users />} />
         </Route>
         <Route path="/register" element={<Register />} />
@@ -45,7 +57,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/policy" element={<Policy />} />
         <Route path="*" element={<PageNotFound />} />
-        <Route path="/pets" element={<Pets />} />
+        {/* <Route path="/pets" element={<Pets />} /> */}
       </Routes>
     </>
   );
---
+++ client/src/pages/CartPage.js
@@ -0,0 +1,16 @@
+import { Layout } from "antd";
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <Layout title={"Liked Pets"}>
+      <div className="container">
+        <div className="row">
+          <h1>Liked Pets</h1>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default CartPage;
