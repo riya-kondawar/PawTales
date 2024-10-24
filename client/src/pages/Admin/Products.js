00e6f71db12bdd0afcb127a12b7bed642a27f791
manasimali57 <manasimali246@gmail.com>
2024-10-24 23:26:16 +0530

    updateM

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -1,49 +1,68 @@
 import React, { useState, useEffect } from "react";
-import AdminMenu from "../components/Layout/AdminMenu";
-import Layout from "../components/Layout/Layout";
-import React from "react"
-import axios from 'axios'
-import toast from 'react-hot-toast'
+import AdminMenu from "../../components/Layout/AdminMenu";
+import Layout from "./../../components/Layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
-const Products = () =>{
-    const[Products,setProducts] = useState([])
+const Products = () => {
+  const [products, setProducts] = useState([]);
 
-    const getAllProducts = async () => {
-        try{
-            const{data} = await axios.get("/api/v1/product/get-product");
-            setProducts(data.Products);
-        } catch(error){
-            console.log(error);
-            toast.error("something went wrong");
-        }
-    };
+  // Fetch all products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
 
-    useEffect(() =>{
-        getAllProducts();
+  // Lifecycle method
+  useEffect(() => {
+    getAllProducts();
+  }, []);
 
-    },[]);
-
-    return(
-        <Layout>
-            <div className= "row"></div>
-            <div className= "col-md-3">
-                <AdminMenu/>
-            </div>
-            <div className="col-md-9">
-                <h1 className="text-center">Cats and Dogs</h1>
-                {product?.map(p =>{
-                     <div className="card" key={p.id}>
-                     <img src= {p.photo} />
-                     <div class="card-details">
-                        <p><strong>Name:</strong> {p.name}</p>
-                        <p><strong>City:</strong> {p.location}</p>
-                     </div>
-                 </div>
-                })}
-            </div>
-            
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div className="row">
+        <div className="col-md-3">
+          <AdminMenu />
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center">All Products List</h1>
+          <div className="d-flex flex-wrap">
+            {products.map((product) => (
+              <Link
+                key={product._id}
+                to={`/dashboard/admin/product/${product.slug}`}
+                className="product-link"
+              >
+                <div className="card m-2" style={{ width: "18rem" }}>
+                  <img
+                    src={`/api/v1/product/product-photo/${product._id}`}
+                    className="card-img-top"
+                    alt={product.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{product.name}</h5>
+                    <p className="card-text">{product.description}</p>
+                    <p><strong>Animal ID:</strong> {product.animalID}</p>
+                    <p><strong>Gender:</strong> {product.gender}</p>
+                    <p><strong>Location:</strong> {product.location}</p>
+                    <p><strong>Sex:</strong> {product.sex}</p>
+                    <p><strong>Date of Birth:</strong> {product.dob ? new Date(product.dob).toLocaleDateString() : "N/A"}</p>
+                    <p><strong>Intake Date:</strong> {new Date(product.intakeDate).toLocaleDateString()}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Products;
