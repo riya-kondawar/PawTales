7517c635292628327fbecf441b6debb597df14fc
riiya-kondawar <riya.kondawar@gmail.com>
2024-10-24 23:34:08 +0530

    updates R

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
 import { useAuth } from "../context/auth";
+import SearchInput from "../components/Form/SearchInput";
 
 const HomePage = () => {
   const [auth, setAuth] = useAuth()
@@ -9,6 +10,8 @@ const HomePage = () => {
       <div className="home">
         <h1>HomePage</h1>
         <pre>{JSON.stringify(auth, null, 4)}</pre>
+
+        <SearchInput />
       </div>
     </Layout>
   );
